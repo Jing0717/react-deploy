93628fdb0acb2b96397e2cd001a5ef1a73476a02
Jing Wu <jingwu@JingdeMacBook-Pro.local>
2020-09-14 12:19:34 +0800

    Updates

---
+++ precache-manifest.6deb4b80dda6c4f306ef8ae3ac60579c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "facede9891190959d034f650882684db",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "b2c673b4a9ccdf829767",
+    "url": "/react-deploy/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/react-deploy/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c673b4a9ccdf829767",
+    "url": "/react-deploy/static/js/main.bd537822.chunk.js"
+  },
+  {
+    "revision": "86d98ee0cb1baf327b93",
+    "url": "/react-deploy/static/js/runtime-main.a7f1a508.js"
+  }
+]);
